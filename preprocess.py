5848db944949aef3f3289af24b705a8f342bcc6b
amtulsadiya <amtulrahmansadiya@gmail.com>
2026-04-10 00:21:34 +0530

    fixed reqs and preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,27 @@
+# import re
+# from nltk.corpus import stopwords
+# import nltk
+# nltk.download('stopwords')
+
+# stop_words = set(stopwords.words('english'))
+
+# def preprocess(text):
+#     text = text.lower()
+#     text = re.sub(r'[^\w\s]', '', text)
+#     words = text.split()
+#     words = [word for word in words if word not in stop_words]
+#     return words
+
 import re
+import nltk
 from nltk.corpus import stopwords
 
-stop_words = set(stopwords.words('english'))
+# Download only if not already present
+try:
+    stop_words = set(stopwords.words('english'))
+except LookupError:
+    nltk.download('stopwords')
+    stop_words = set(stopwords.words('english'))
 
 def preprocess(text):
     text = text.lower()
