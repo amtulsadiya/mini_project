eef35892d38bb11493d24d80b5574805c73aae48
Amtul Sadiya <159041572+amtulsadiya@users.noreply.github.com>
2026-04-19 12:13:10 +0530

    Clean up preprocess.py by removing comments
    Removed commented-out code for preprocessing text.

--- preprocess.py
+++ preprocess.py
@@ -1,16 +1,4 @@
-# import re
-# from nltk.corpus import stopwords
-# import nltk
-# nltk.download('stopwords')
 
-# stop_words = set(stopwords.words('english'))
-
-# def preprocess(text):
-#     text = text.lower()
-#     text = re.sub(r'[^\w\s]', '', text)
-#     words = text.split()
-#     words = [word for word in words if word not in stop_words]
-#     return words
 
 import re
 import nltk
